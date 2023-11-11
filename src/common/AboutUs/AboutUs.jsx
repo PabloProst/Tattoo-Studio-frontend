ef480af5c3b68256d31242e03432cce3618f0486
PabloProst <pabloprost16@gmail.com>
2023-11-11 19:27:21 +0100

    feat: aboutUs design done

--- src/common/AboutUs/AboutUs.jsx
+++ src/common/AboutUs/AboutUs.jsx
@@ -7,8 +7,8 @@ export const AboutUs = () => {
             <div className='aboutUsDiv'>
                 <img className='aboutUsLogo' src='../src/assets/img/tattooLogo.png' alt="Logo" />
                 <div className='textAboutUs'>
-                    <p>Dynamic Ink Tattoo Studio, nestled in Benimaclet, Valencia, offers a distinctive tattooing experience. Since 2014, we seamlessly merge the authenticity of traditional studios with modern facilities and an exceptional artistic team.</p>
-                    <p>Committed to delivering top-quality tattoos with premium materials and personalized service, Dynamic Ink guides you from custom design to aftercare. Our passion for tattooing shines in every project as we evolve, embracing new creative challenges. Join us at Dynamic Ink for an exceptional tattooing experience!</p>
+                    <p><span className='colourText'>Dynamic Ink Tattoo Studio</span>, nestled in Benimaclet, Valencia, offers a distinctive tattooing experience. Since <span className='colourText'>2014</span>, we seamlessly merge the authenticity of traditional studios with modern facilities and an exceptional <span className='colourText'>artistic team</span>.</p>
+                    <p>Committed to delivering top-quality tattoos with <span className='colourText'>premium materials</span> and personalized service, Dynamic Ink guides you from custom design to aftercare. Our <span className='colourText'>passion</span> for tattooing shines in every project as we evolve, embracing new creative challenges. Join us at Dynamic Ink for an exceptional tattooing <span className='colourText'>experience</span>!</p>
                 </div>
             </div>
         </div>
