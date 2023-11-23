47d8b918d2d29c32a4985585989b5ee61f379541
PabloProst <pabloprost16@gmail.com>
2023-11-23 09:21:23 +0100

    modified crew page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,12 @@
 import {React} from 'react';
 import "./Home.css";
 import { Banner } from '../../common/BannerHome/BannerHome';
-import { AboutUs } from '../../common/AboutUs/AboutUs';
 import { Information } from '../../common/Information/Information';
-import { TattooStyles } from '../../common/TattooStyles/TattooStyles';
-import { StaticNavbar } from '../../common/StaticNavbar/StaticNavbar';
 
 export const Home = () => {
   return (
     <div>
       <div><Banner/></div>
-      <div><AboutUs/></div>
-      <div><TattooStyles/></div>
       <div><Information/></div>
     </div>
   );
